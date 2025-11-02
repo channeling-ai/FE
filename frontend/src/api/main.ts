c97cdf43fed5dbf472707f881d143e388f44dee6
Sowon Kim <sso626262@naver.com>
2025-11-02 22:22:25 +0900

    feat: 인기있는 영상의 비결은? 더미 영상 리스트 조회 API 연결

--- frontend/src/api/main.ts
+++ frontend/src/api/main.ts
@@ -1,13 +1,18 @@
+import type { RecommendedMyVideosDto, ResponseRecommendedDummyVideos, ResponseRecommendedMyVideos } from '../types/main'
 import { axiosInstance } from './axios'
-import type { RecommendedVideosDto, ResponseRecommededVideos } from '../types/main'
 
-export const getRecommededVideos = async ({
+export const getRecommededMyVideos = async ({
     channelId,
     page,
     size,
-}: RecommendedVideosDto): Promise<ResponseRecommededVideos> => {
+}: RecommendedMyVideosDto): Promise<ResponseRecommendedMyVideos> => {
     const { data } = await axiosInstance.get(`channels/${channelId}/recommended-videos`, {
         params: { page, size },
     })
+    return data
+}
+
+export const getRecommededDummyVideos = async (): Promise<ResponseRecommendedDummyVideos> => {
+    const { data } = await axiosInstance.get(`/dummies/videos`)
     return data
 }
