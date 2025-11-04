09e4eda2d0983fe2ef9b8c46c5a89658e18d56c2
yoonvinjeong <anaabc1004@naver.com>
2025-11-04 14:15:18 +0900

    feat: 아이디어페이지 api 연결

---
+++ frontend/src/hooks/idea/usePostIdea.ts
@@ -0,0 +1,20 @@
+import { useMutation, useQueryClient } from '@tanstack/react-query'
+import { postIdea } from '../../api/idea'
+import type { PostIdeaDto, ResponsePostIdea } from '../../types/idea'
+import type { AxiosError } from 'axios'
+
+export default function usePostIdea() {
+    const queryClient = useQueryClient()
+    return useMutation<ResponsePostIdea, AxiosError<ResponsePostIdea>, PostIdeaDto>({
+        mutationFn: postIdea,
+        mutationKey: ['postIdea'],
+        onSuccess: (data) => {
+            console.log('아이디어생성 성공: ', data)
+            queryClient.invalidateQueries({ queryKey: ['ideas'] })
+        },
+        onError: (error) => {
+            const errorMessage = error.response?.data?.message
+            console.error('아이디어 생성 실패:', errorMessage)
+        },
+    })
+}
